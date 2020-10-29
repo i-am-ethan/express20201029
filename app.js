b8a9839300b07d22c05ad4c7bbfae97caf93b03b
ethan <izawaryosuke0101@gmail.com>
2020-10-29 12:48:30 +0900

    get

--- app.js
+++ app.js
@@ -7,7 +7,22 @@ app.get('/', function(req,res){
 app.get('/about', function(req,res){
   res.send('about now');
 });
+app.get('/users/:name', function(req,res){
+  res.send(req.params.name);
+});
+app.get('/users/:name?', function(req,res){
+  if(req.params.name){
+    res.send(req.params.name);
+  }else{
+    res.send("hello nobody");
+  }
+});
+
+app.get('/items/:id([0-9]+)', function(req,res){
+  res.send(req.params.id);
+});
 
 app.listen(3000);
 console.log("server running");
+
 
